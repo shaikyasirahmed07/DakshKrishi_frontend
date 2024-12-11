1f07d60d430d9c0e7c11760f1e8f0a51fe27f347
YASIR AHMED SHAIK <147685813+shaikyasirahmed07@users.noreply.github.com>
2024-12-11 17:22:18 +0530

    Update OrderPage.jsx

--- src/user/pages/OrderPage.jsx
+++ src/user/pages/OrderPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import Navbar from '../components/NavBar';
+import Navbar from '../components/Navbar';
 import '../pages/css/OrderPage.css'
 
 const OrdersPage = () => {
